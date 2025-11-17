02bc008a6b85fa11fb7811d44799d4fbf66d3e9c
Navyasinghal1234 <write24navya@gmail.com>
2025-11-18 00:23:07 +0530

    q52.c

---
+++ q52.c
@@ -0,0 +1,24 @@
+//WAP TO PRINT PATTERN.
+#include <stdio.h>
+
+int main() {
+    printf("*\n\n");     // first star and one blank line
+
+    printf("*\n");       // block 1
+    printf("*\n");
+    printf("*\n\n");     // blank line after block
+
+    printf("*\n");       // block 2
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n\n");     // blank line
+
+    printf("*\n");       // block 3
+    printf("*\n");
+    printf("*\n\n");     // blank line
+
+    printf("*\n");       // last single star
+
+    return 0;
+}
